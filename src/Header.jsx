eb50ec8a8edfb1ab110a597594cb637016d343ee
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-07-11 20:58:14 +0200

    Header_section_Bulding

---
+++ src/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__container">
+                <div className="header__content">
+                    <a href="#" className="header__logo">
+                        <span className="header__logo-white">Dev</span>Freelancer
+                    </a>
+
+                    <button id="mobile-menu-button" className="header__menu-button">
+                        <i className="fas fa-bars header__menu-icon"></i>
+                    </button>
+
+                    <nav className="header__nav">
+                        <a href="#about" className="nav-link">O mnie</a>
+                        <a href="#services" className="nav-link">Usługi</a>
+                        <a href="#projects" className="nav-link">Projekty</a>
+                        <a href="#contact" className="nav-link">Kontakt</a>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header;

3d4a29cd15cb1b7dea101166739f15419fac80f9
dawid24214 <155809372+dawid24214@users.noreply.github.com>
2025-07-23 11:07:01 +0200

    .change

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -3,9 +3,9 @@ import {useEffect, useState} from 'react'
    const Test = () => {
     return (
         <>
-            <button className="bg-indigo-600 hover:bg-indigo-700 text-white px-6 py-2 rounded-md shadow transition">
-                Zobacz więcej
-            </button>
+            <h1 className="">
+                Witaj na mojej stronie!
+            </h1>
 
         </>
     )

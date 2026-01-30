dcb6d998a279b297ef7baa9caa804758f49da7a3
Dawid Grochowski <155809372+dawid24214@users.noreply.github.com>
2026-01-30 10:48:21 +0100

    App
    Adding ReactDom navigation points

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,3 @@
-/*
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ''
-
-function App() {
-  const [count, setCount] = useState(0)
-*/
-
 import { useEffect , useMemo , useState } from "react";
 import SideBar from "./components/Sidebar.jsx";
 import Hero from "./components/Hero.jsx";
@@ -20,3 +10,46 @@ import Contact from "./components/Contact.jsx";
 import Certification from "./components/Certification.jsx";
 import Footer from "./components/Footer.jsx";
 
+export default function App() {
+    const [sidebarHidden, setSidebarHidden] = useState(false);
+
+
+
+    useEffect(() => {
+        try {
+            const saved = localStorage.getItem("sidebarHidden");
+            // eslint-disable-next-line react-hooks/set-state-in-effect
+            if (saved === "1") setSidebarHidden(true);
+
+        } catch {''}
+
+    }, [sidebarHidden]);
+    const year = useMemo(() => new Date().getFullYear(), []);
+
+    return (
+        <>
+        <div className = 'grid-bg' aria-hidden='true'/>
+
+            <SideBar
+            hidden={sidebarHidden}
+            onToggle={() => setSidebarHidden((v) => !v)}
+                onOpen={()=>setSidebarHidden(false)}
+            />
+
+               <main className={`main ${sidebarHidden ? 'main--hidden': ''}`} id='top'>
+                   <Hero/>
+                   <About/>
+                   <Skills/>
+                   <Projects/>
+                   <WhyDev/>
+                   <TimeLine/>
+                   <Contact/>
+                   <Certification/>
+               </main>
+
+            <Footer year={year} />
+
+        </>
+    );
+
+}
